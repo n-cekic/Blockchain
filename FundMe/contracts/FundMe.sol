bb16bdda36b6f23170256b3423816c1adfaa52c2
n-cekic <ncekic13@gmail.com>
2023-03-15 17:50:56 +0100

    create FundMe constract

--- FundMe/contracts/FundMe.sol
+++ FundMe/contracts/FundMe.sol
@@ -16,12 +16,15 @@ contract FundMe {
     address public /* immutable */ i_owner;
     uint256 public constant MINIMUM_USD = 50 * 10 ** 18;
     
-    constructor() {
+    AggregatorV3Interface public priceFeed;
+
+    constructor(address addr) {
         i_owner = msg.sender;
+        priceFeed = AggregatorV3Interface(addr);
     }
 
     function fund() public payable {
-        require(msg.value.getConversionRate() >= MINIMUM_USD, "You need to spend more ETH!");
+        require(msg.value.getConversionRate(priceFeed) >= MINIMUM_USD, "You need to spend more ETH!");
         // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
         addressToAmountFunded[msg.sender] += msg.value;
         funders.push(msg.sender);
@@ -29,7 +32,6 @@ contract FundMe {
     
     function getVersion() public view returns (uint256){
         // ETH/USD price feed address of Sepolia Network.
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         return priceFeed.version();
     }
     
