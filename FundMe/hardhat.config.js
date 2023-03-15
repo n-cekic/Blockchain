28cb5e5f0c05beab5bbfcfea3f7b21902e53b299
n-cekic <ncekic13@gmail.com>
2023-03-15 17:52:02 +0100

    edit hardhat config

--- FundMe/hardhat.config.js
+++ FundMe/hardhat.config.js
@@ -1,7 +1,28 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox")
 require("hardhat-deploy")
+require("dotenv").config()
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
-};
+    solidity: {
+        compilers: [{ version: "0.8.18" }, { version: "0.6.6" }],
+    },
+    networks: {
+		hardhat: {
+            chainId: 31337,
+        },
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 5,
+        },
+    },
+	namedAccounts: {
+		deployer: {
+			default: 0,
+		},
+	},
+}
