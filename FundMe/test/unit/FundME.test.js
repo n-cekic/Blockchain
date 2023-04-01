bcbb87fddf6a0598555a1fa79a5ec7ba3b6e138f
n-cekic <ncekic13@gmail.com>
2023-04-02 00:21:53 +0200

    add withdraw test for single funder

--- FundMe/test/unit/FundME.test.js
+++ FundMe/test/unit/FundME.test.js
@@ -1,12 +1,16 @@
 const { assert, expect } = require("chai")
 const { deployments, ethers, getNamedAccounts } = require("hardhat")
-const { sortAndDeduplicateDiagnostics, isConstructSignatureDeclaration } = require("typescript")
+const {
+    sortAndDeduplicateDiagnostics,
+    isConstructSignatureDeclaration,
+    EndOfLineState,
+} = require("typescript")
 
 describe("FundMe", function () {
     let fundMe, deployer, mockV3Aggregator
-    const sendValue = "100000000000000000" //ethers.utils.parseEther("1")
+    const sendValue = ethers.utils.parseEther("1")
 
-    this.beforeEach(async function() {
+    this.beforeEach(async function () {
         deployer = (await getNamedAccounts()).deployer
         await deployments.fixture(["all"])
         fundMe = await ethers.getContract("FundMe", deployer)
@@ -16,26 +20,59 @@ describe("FundMe", function () {
         )
     })
 
-    describe("constructor", async function() {
-        it("sets the aggregator addresses correctly", async function() {
+    describe("constructor", async function () {
+        it("sets the aggregator addresses correctly", async function () {
             const response = await fundMe.getPriceFeed()
             assert.equal(response, mockV3Aggregator.address)
         })
     })
 
-    describe("fund", async function() {
-        it("tests for sufficient funds", async function() {
+    describe("fund", async function () {
+        it("tests for sufficient funds", async function () {
             await expect(fundMe.fund()).to.be.reverted
         })
-        it("update the amount funded data structure", async function() {
+        it("update the amount funded data structure", async function () {
             await fundMe.fund({ value: sendValue })
             const resp = await fundMe.getAddressToAmountFunded(deployer)
             assert.equal(resp.toString(), sendValue.toString())
         })
-        it("adds new funder", async function() {
-            await fundMe.fund({value: sendValue})
+        it("adds new funder", async function () {
+            await fundMe.fund({ value: sendValue })
             const resp = await fundMe.getFunder(0)
             assert.equal(resp, deployer)
+        })
+    })
+    describe("withdraw", async function () {
+        this.beforeEach(async function () {
+            await fundMe.fund({ value: sendValue })
+        })
+
+        it("withdraw ETH from a single funder", async function () {
+            const startingContractBalance = await fundMe.provider.getBalance(
+                fundMe.address
+            )
+            const startingDeployerBalance = await fundMe.provider.getBalance(
+                deployer
+            )
+
+            const transactionResponse = await fundMe.withdraw()
+            const transactionReceipt = await transactionResponse.wait(1)
+            
+            const {gasUsed, effectiveGasPrice} = transactionReceipt
+            const gasCost = gasUsed.mul(effectiveGasPrice)
+
+            const endContractBalance = await fundMe.provider.getBalance(
+                fundMe.address
+            )
+            const endDeployerBalance = await fundMe.provider.getBalance(
+                deployer
+            )
+
+            assert.equal(endContractBalance, 0)
+            assert.equal(
+                startingContractBalance.add(startingDeployerBalance).toString(),
+                endDeployerBalance.add(gasCost).toString()
+            )
         })
     })
 })
