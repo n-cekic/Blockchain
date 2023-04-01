1e7156425548c2f658c838dc9393e9ff3612810b
n-cekic <ncekic13@gmail.com>
2023-04-01 23:49:47 +0200

    add test for new funder being added to funders list

--- FundMe/test/unit/FundME.test.js
+++ FundMe/test/unit/FundME.test.js
@@ -1,12 +1,12 @@
 const { assert, expect } = require("chai")
 const { deployments, ethers, getNamedAccounts } = require("hardhat")
-const { sortAndDeduplicateDiagnostics } = require("typescript")
+const { sortAndDeduplicateDiagnostics, isConstructSignatureDeclaration } = require("typescript")
 
 describe("FundMe", function () {
     let fundMe, deployer, mockV3Aggregator
     const sendValue = "100000000000000000" //ethers.utils.parseEther("1")
 
-    this.beforeEach(async function () {
+    this.beforeEach(async function() {
         deployer = (await getNamedAccounts()).deployer
         await deployments.fixture(["all"])
         fundMe = await ethers.getContract("FundMe", deployer)
@@ -16,22 +16,26 @@ describe("FundMe", function () {
         )
     })
 
-    describe("constructor", async function () {
-        it("sets the aggregator addresses correctly", async function () {
+    describe("constructor", async function() {
+        it("sets the aggregator addresses correctly", async function() {
             const response = await fundMe.getPriceFeed()
             assert.equal(response, mockV3Aggregator.address)
         })
     })
 
-    describe("fund", async function () {
-        it("tests for sufficient funds", async function () {
+    describe("fund", async function() {
+        it("tests for sufficient funds", async function() {
             await expect(fundMe.fund()).to.be.reverted
         })
-        it("update the amount funded data structure", async function () {
+        it("update the amount funded data structure", async function() {
             await fundMe.fund({ value: sendValue })
-
             const resp = await fundMe.getAddressToAmountFunded(deployer)
             assert.equal(resp.toString(), sendValue.toString())
+        })
+        it("adds new funder", async function() {
+            await fundMe.fund({value: sendValue})
+            const resp = await fundMe.getFunder(0)
+            assert.equal(resp, deployer)
         })
     })
 })
